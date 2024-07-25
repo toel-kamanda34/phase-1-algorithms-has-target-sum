65d2767a59e3a539da18fe0115918c070d17cff7
tkamanda <toelokemwa@gmail.com>
2024-07-25 04:58:33 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
 
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 /* 
-  Write the Big O time complexity of your function here
+   Big O time complexity: O(n^2)
+  n - length of the input array
 */
 
 /* 
-  Add your pseudocode here
+1. Start with the first number in the list.
+2. Add it to every other number that comes after it, one by one.
+3. If any of these pairs add up to our target number, we've found what we're looking for! We can stop and say "Yes, we found it!"
+4. If we didn't find a match, move to the second number and repeat steps 2-3.
+5. Keep doing this until we've tried all possible pairs.
+6. If we go through everything and don't find a match, we have to say "Nope, couldn't find it."
+
 */
 
 /*
-  Add written explanation of your solution here
+I'm picking up each card, one at a time, and adding it to every other card on the table.
+If I find two cards that add up to our target number, great! I stop there and say "found a match."
+If I go through all the cards and can't find a pair that adds up to my target, then I have to say there isn't a match.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +46,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+   // Additional test cases
+   console.log("");
+   console.log("Expecting: true");
+   console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+  
+   console.log("");
+   console.log("Expecting: false");
+   console.log("=>", hasTargetSum([4], 5));
 }
 
 module.exports = hasTargetSum;
